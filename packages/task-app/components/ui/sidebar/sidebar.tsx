1a0088dcaadee5d84e5e8d21b76ec7dc747ae389
NicolasCarriman <nicolascarriman045@gmail.com>
2023-06-23 19:27:56 -0300

    fix(app): fix ui

--- packages/task-app/components/ui/sidebar/sidebar.tsx
+++ packages/task-app/components/ui/sidebar/sidebar.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { SideBarContainer, SidebarComponent, SidebarFooter, SidebarHead, SidebarMainContainer } from './sidebar.styled';
 import { CurrentProject } from '../../common/projectName';
 import ButtonIcon from '../button-icon/buttonIcon';
 import AvatarComponent from '../../common/avatar';
-import { IUser, store } from '@core/index';
 import SideBarLink from '@app/components/common/link';
-import useLocalStorage from '@app/hooks/useLocalStorage';
+import Link from 'next/link';
 
 /**
  * todo: fix navigation structure
@@ -15,15 +14,7 @@ import useLocalStorage from '@app/hooks/useLocalStorage';
  */
 
 function Sidebar() {
-
   const [active, setActive] = useState(false);
-  // eslint-disable-next-line no-unused-vars
-  const [project, setProject] = useLocalStorage('project', {});
-  // eslint-disable-next-line no-unused-vars
-  const [user, setUser] = useLocalStorage('user', {});
-
-  const projectName = store.getState().project.name;
-  const avatar = store.getState().user as IUser;
 
   const onHover = () => {
     setActive(true);
@@ -33,22 +24,13 @@ function Sidebar() {
     setActive(false);
   };
 
-  useEffect(() => {
-    projectName && setProject(store.getState().project);
-    avatar.name.length && setUser(store.getState().user);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [projectName, avatar]);
-
   return (
     <SideBarContainer>
       <SidebarComponent onHover={onHover} onLeave={onLeave}>
         <SidebarHead>
-          <SideBarLink href='./dashboard'>
-            {
-              projectName &&
-              <CurrentProject project={projectName} />
-            }
-          </SideBarLink>
+          <Link href='./dashboard' className='w-full font-bold text-start'>
+            My Company
+          </Link>
         </SidebarHead>
         <SidebarMainContainer>
           <SideBarLink href={'./dashboard/tasks'} >
@@ -60,12 +42,9 @@ function Sidebar() {
           <ButtonIcon type='team' showText={active} disabled />
           <ButtonIcon type='calendar' showText={active} disabled />
         </SidebarMainContainer>
-        {
-          avatar &&
-          <SidebarFooter spanLabel={avatar.name}>
-            <AvatarComponent isCurrentUser={true} label={avatar.name} />
+          <SidebarFooter spanLabel={'Emily Brown'}>
+            <AvatarComponent isCurrentUser={true} label={'Emily Brown'} />
           </SidebarFooter>
-        }
       </SidebarComponent>
     </SideBarContainer>
   );
