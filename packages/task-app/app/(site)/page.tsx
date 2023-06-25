4890bf25378230a9427b34918f416ed147298b61
nicolas carriman <89722163+NicolasCarriman@users.noreply.github.com>
2023-06-25 09:02:50 -0300

    Update page.tsx

--- packages/task-app/app/(site)/page.tsx
+++ packages/task-app/app/(site)/page.tsx
@@ -5,11 +5,8 @@ import Image from 'next/image';
 import dino from '../../public/dino2.jpg';
 import SideBarLink from '@app/components/common/link';
 import Button from '@app/components/common/button';
-import { getTasks } from '@core/services/getTasks';
 
 async function Home() {
-  const data = await  getTasks();
-
   return (
     <main className='w-full h-full flex flex-col gap-3'>
       <div className={twMerge('flex w-100% justify-center items-center mt-10', style.slideAppearAnimation)}>
@@ -36,9 +33,6 @@ async function Home() {
               </p>
             </Button>
           </SideBarLink>
-        </div>
-        <div>
-        {data.currentTask}
         </div>
         <div className='flex justify-center items-cente shadow-2xl rounded-lg p-4 z-50'>
           <Image src={dino} alt={'dinosaur'} height='200' width='400' />
