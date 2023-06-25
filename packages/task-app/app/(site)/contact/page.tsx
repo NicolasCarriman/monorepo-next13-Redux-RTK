ee36cd0a5690a9fc5ec5a1ceb651d56b7ca725ec
NicolasCarriman <nicolascarriman045@gmail.com>
2023-06-25 13:06:41 -0300

    feat(site): completed landing page
    add diferents sections to landing page

---
+++ packages/task-app/app/(site)/contact/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PageWrapper } from '../components/wrapperAnmiation';
+
+function Contact() {
+  return (
+    <PageWrapper>
+      <section>
+        <h1>Contacto</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </section>
+    </PageWrapper>
+  );
+}
+
+export default Contact;
---
+++ packages/task-app/app/(site)/product/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PageWrapper } from '../components/wrapperAnmiation';
+import SideBarLink from '@app/components/common/link';
+import Button from '@app/components/common/button';
+
+function Product() {
+  return (
+    <PageWrapper>
+      <section className='flex flex-col gap-4 justify-center items-center'>
+        <p className='font-semibold'>
+          Únete a nosotros y experimenta una forma más eficiente de trabajar
+        </p>
+        <p>
+          Con características innovadoras que facilita la colaboración en equipo y fomenta la eficiencia en cada proceso.
+          nuestro producto se adapta a tus necesidades, ofreciendo una experiencia fluida y adaptativa.
+        </p>
+        <SideBarLink href='./dashboard'>
+          <Button size={'small'}>
+            <p className='p-2 whitespace-nowrap'>
+              Prueba Ahora!
+            </p>
+          </Button>
+        </SideBarLink>
+      </section>
+    </PageWrapper>
+  );
+}
+
+export default Product;
