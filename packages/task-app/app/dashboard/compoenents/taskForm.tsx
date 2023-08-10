18d0ad0957fc14a0d71ef7b319df7fc0f7992397
nicolas <lnicolas62@yahoo.com>
2023-08-10 04:10:33 -0300

    feat: d

--- packages/task-app/app/dashboard/compoenents/taskForm.tsx
+++ packages/task-app/app/dashboard/compoenents/taskForm.tsx
@@ -1,24 +1,32 @@
-'use client';
-import { TaskPriority, TaskTags } from '@app/components/common';
-import AvatarComponent from '@app/components/common/avatar';
-import ButtonComponent from '@app/components/common/button';
-import Button from '@app/components/common/button';
-import Input from '@app/components/common/input';
-import List from '@app/components/common/list';
-import ListItem from '@app/components/common/listItem';
-import InputSearch, { onClickCallBack } from '@app/components/ui/inputSearch/inputSearch';
-import { useAppSelector } from '@app/hooks/redux';
-import { useProject } from '@app/hooks/useProject';
-import { useTask } from '@app/hooks/useTasks';
-import { useTeam } from '@app/hooks/useTeam';
-import { getRandomId } from '@app/utils';
-import { taskPriorities } from '@app/utils/priority';
-import { ITask, ITeam, ITeamCategory, ProjectTeam, category, priorityType, IUser } from '@core/models';
-import { projectSelector } from '@core/redux/reducers/projectSlice/project.selector';
-import React, { useState } from 'react';
-import StyledWrapper from "./styleWrapper"
-
-
+"use client";
+import { TaskPriority, TaskTags } from "@app/components/common";
+import AvatarComponent from "@app/components/common/avatar";
+import ButtonComponent from "@app/components/common/button";
+import Button from "@app/components/common/button";
+import Input from "@app/components/common/input";
+import List from "@app/components/common/list";
+import ListItem from "@app/components/common/listItem";
+import InputSearch, {
+  onClickCallBack,
+} from "@app/components/ui/inputSearch/inputSearch";
+import { useAppSelector } from "@app/hooks/redux";
+import { useProject } from "@app/hooks/useProject";
+import { useTask } from "@app/hooks/useTasks";
+import { useTeam } from "@app/hooks/useTeam";
+import { getRandomId } from "@app/utils";
+import { taskPriorities } from "@app/utils/priority";
+import {
+  ITask,
+  ITeam,
+  ITeamCategory,
+  ProjectTeam,
+  category,
+  priorityType,
+  IUser,
+} from "@core/models";
+import { projectSelector } from "@core/redux/reducers/projectSlice/project.selector";
+import React, { useState } from "react";
+import StyledWrapper from "./styleWrapper";
 
 interface TaskFormModal {
   closeModal: () => void;
@@ -27,15 +35,23 @@ interface TaskFormModal {
 }
 
 function TaskForm({
-  closeModal, setTaskName, setTaskDescription
+  closeModal,
+  setTaskName,
+  setTaskDescription,
 }: TaskFormModal) {
   const [error, setError] = useState<undefined | string>(undefined);
-  const [members, setMembers] = useState<{ name: string, id: string }[]>([]);
-  const [selectedTeam, setSelectedTeam] = useState<{ name: string, id: string }>({
-    name: '',
-    id: '',
+  const [members, setMembers] = useState<{ name: string; id: string }[]>([]);
+  const [selectedTeam, setSelectedTeam] = useState<{
+    name: string;
+    id: string;
+  }>({
+    name: "",
+    id: "",
   });
-  const [category, setCategory] = useState<{ name: string, id: string }>({ name: '', id: '' });
+  const [category, setCategory] = useState<{ name: string; id: string }>({
+    name: "",
+    id: "",
+  });
   const [priority, setPriority] = useState<priorityType>();
   const { team, updateTeamCategory, addNewCategory } = useTeam();
   const { addNewTeam } = useProject();
@@ -44,25 +60,24 @@ function TaskForm({
 
   const users = project.users as IUser[];
   const teams = project.teams as ProjectTeam[];
-  const currentTeam = selectedTeam && teams?.find((t: ProjectTeam) => t.id === selectedTeam.id);
-  const categories: category[] | undefined = currentTeam && currentTeam.categories;
+  const currentTeam =
+    selectedTeam && teams?.find((t: ProjectTeam) => t.id === selectedTeam.id);
+  const categories: category[] | undefined =
+    currentTeam && currentTeam.categories;
   const isNewTeam = !currentTeam;
   const priorities = taskPriorities;
 
   const updateCategory = (newTask: ITask) => {
-    const currentCategory = team.teamCategory.find((c) => c.categoryid === category.id);
+    const currentCategory = team.teamCategory.find(
+      (c) => c.categoryid === category.id
+    );
     if (!currentCategory) return;
     const updatedCategory: ITeamCategory = {
       name: currentCategory.name,
-      tasks: [...currentCategory.tasks,
-      newTask.taskId
-      ],
-      usersId: [
-        ...currentCategory.usersId,
-        ...members.map((m) => m.id)
-      ],
+      tasks: [...currentCategory.tasks, newTask.taskId],
+      usersId: [...currentCategory.usersId, ...members.map((m) => m.id)],
       categoryid: currentCategory.categoryid,
-      goals: currentCategory.goals
+      goals: currentCategory.goals,
     };
 
     updateTeamCategory(updatedCategory);
@@ -72,9 +87,9 @@ function TaskForm({
     const newCategory: ITeamCategory = {
       name: categoryName,
       tasks: [taskId],
-      usersId: members.map(m => m.id),
+      usersId: members.map((m) => m.id),
       categoryid: getRandomId(),
-      goals: []
+      goals: [],
     };
     addNewCategory(newCategory);
   };
@@ -86,13 +101,12 @@ function TaskForm({
       categories: [
         {
           name: categoryName,
-          id: getRandomId()
-        }
-      ]
+          id: getRandomId(),
+        },
+      ],
     };
 
     addNewTeam(newTeam);
-      
   };
 
   const handleUsers = (name: string, id: string, fn: onClickCallBack) => {
@@ -101,16 +115,13 @@ function TaskForm({
       id,
     };
     fn(name);
-    if (members.find(m => m.id === id)) return;
+    if (members.find((m) => m.id === id)) return;
 
-    setMembers([
-      ...members,
-      newMember,
-    ]);
+    setMembers([...members, newMember]);
   };
 
   const deleteMembers = (id: string) => {
-    const filtered = members.filter(member => member.id !== id);
+    const filtered = members.filter((member) => member.id !== id);
     setMembers(filtered);
   };
 
@@ -130,7 +141,11 @@ function TaskForm({
     });
   };
 
-  const handlePriority = (name: priorityType, id: string, fn: onClickCallBack) => {
+  const handlePriority = (
+    name: priorityType,
+    id: string,
+    fn: onClickCallBack
+  ) => {
     fn(name);
     setPriority(name);
   };
@@ -143,146 +158,154 @@ function TaskForm({
     let formValues: { [keyof: string]: FormDataEntryValue } = {};
 
     if (!priority) {
-      setError('Invalid priority');
+      setError("Invalid priority");
       return;
     } else setError(undefined);
     if (members.length === 0) {
-      setError('add members');
+      setError("add members");
       return;
     } else setError(undefined);
-
 
     for (let [name, value] of formData.entries()) {
       formValues[name] = value;
     }
 
     const newTask: ITask = {
-      taskName: formValues['taskName'] as string,
+      taskName: formValues["taskName"] as string,
       taskDate: today,
-      taskDescription: formValues['taskDescription'] as string,
-      taskCategory: formValues['category'] as string || category.id,
+      taskDescription: formValues["taskDescription"] as string,
+      taskCategory: (formValues["category"] as string) || category.id,
       taskPriority: priority,
       taskId: getRandomId(),
-      taskStatus: 'inProgress',
-      taskUsersId: members.map(m => m.id),
-      subtasks: []
+      taskStatus: "inProgress",
+      taskUsersId: members.map((m) => m.id),
+      subtasks: [],
     };
-    const isNewCategory = formValues['category'] as string !== category.name;
+    const isNewCategory = (formValues["category"] as string) !== category.name;
 
     if (!isNewTeam && !isNewCategory) updateCategory(newTask);
-    if (!isNewTeam && isNewCategory) addCategory(newTask.taskId, formValues['category'] as string);
-    if (isNewTeam) addTeam(formValues['team'] as string, formValues['category'] as string);
+    if (!isNewTeam && isNewCategory)
+      addCategory(newTask.taskId, formValues["category"] as string);
+    if (isNewTeam)
+      addTeam(formValues["team"] as string, formValues["category"] as string);
     addNewTask(newTask);
     closeModal();
   };
-  
 
   return (
     <form onSubmit={handleSubmit}>
-        <div className='gap-6 sm:flex-row-reverse bg-white p-6 shadow-lg'>
-            <div className='flex-col gap-3 max-w-[45vh]'>
+      <div className="gap-6 sm:flex-row-reverse bg-white p-6 shadow-lg">
+        <div className="flex-col gap-3 max-w-[45vh]">
+          <div className="mb-4">
+            <p className="font-semibold text-lg mb-2">Members:</p>
+            <List
+              className="flex flex-wrap border-none gap-1"
+              data={members}
+              renderedItem={(item) => (
+                <TaskTags variant="small">
+                  {item.name}
+                  <span
+                    onClick={() => deleteMembers(item.id)}
+                    className="ml-2 font-medium cursor-pointer"
+                  >
+                    X
+                  </span>
+                </TaskTags>
+              )}
+            />
+          </div>
 
-               
-                    <div className="mb-4">
-                        <p className="font-semibold text-lg mb-2">Members:</p>
-                        <List
-                            className='flex flex-wrap border-none gap-1'
-                            data={members}
-                            renderedItem={(item) => (
-                                <TaskTags variant='small'>
-                                    {item.name}
-                                    <span onClick={() => deleteMembers(item.id)} className='ml-2 font-medium cursor-pointer'>
-                                        X
-                                    </span>
-                                </TaskTags>
-                            )}
-                        />
-                    </div>
-                
-
-                <StyledWrapper>
-                    <InputSearch
-                        placeHolder='Users'
-                        data={users}
-                        render={(user, fn) => (
-                            <ListItem onClick={() => handleUsers(user.name, user.id, fn)}>
-                                <AvatarComponent variant='small' label={user.name} />
-                                <p className='ml-2 font-medium'>{user.name}</p>
-                                <p className='ml-2 font-light'>{user.departament}</p>
-                            </ListItem>
-                        )}
-                    />
-                </StyledWrapper>
-
-                <StyledWrapper>
-                    <InputSearch
-                        name='team'
-                        placeHolder='Team'
-                        data={teams}
-                        render={(team, fn) => (
-                            <ListItem onClick={() => handleTeam(team.departament, team.id, fn)}>
-                                <p>{team.departament}</p>
-                            </ListItem>
-                        )}
-                    />
-                </StyledWrapper>
-
-                <StyledWrapper >
-                    {!isNewTeam ? (
-                        <InputSearch 
-                            name='category'
-                            placeHolder='Category'
-                            data={categories ? categories : []}
-                            render={(category, fn) => (
-                                <ListItem onClick={() => handleCategory(category.name, category.id, fn)}>
-                                    <p>{category.name}</p>
-                                </ListItem>
-                            )}
-                        />
-                    ) : (
-                        <Input name='category' placeholder='Category' required />
-                    )}
-                </StyledWrapper>
-
-            </div>
-
-            <StyledWrapper>
-                <Input 
-                    name='taskName' 
-                    required 
-                    placeholder='Task Name' 
-                    setValue={setTaskName}
-                />
-            </StyledWrapper>
-
-            <StyledWrapper>
-                <Input 
-                    name='taskDescription' 
-                    required 
-                    placeholder='Task Description' 
-                    setValue={setTaskDescription}
-                />
-            </StyledWrapper>
-
-            <StyledWrapper>
-                <InputSearch
-                    placeHolder='Priority'
-                    data={priorities}
-                    render={(priority, fn) => (
-                        <ListItem onClick={() => handlePriority(priority.name, priority.id, fn)}>
-                            <TaskPriority priority={priority.name} />
-                        </ListItem>
-                    )}
-                />
-            </StyledWrapper>
+          <StyledWrapper>
+            <InputSearch
+              placeHolder="Users"
+              data={users}
+              render={(user, fn) => (
+                <ListItem onClick={() => handleUsers(user.name, user.id, fn)}>
+                  <AvatarComponent variant="small" label={user.name} />
+                  <p className="ml-2 font-medium">{user.name}</p>
+                  <p className="ml-2 font-light">{user.departament}</p>
+                </ListItem>
+              )}
+            />
+          </StyledWrapper>
+          <StyledWrapper>
+            <InputSearch
+              name="team"
+              placeHolder="Team"
+              data={teams}
+              render={(team, fn) => (
+                <ListItem
+                  onClick={() => handleTeam(team.departament, team.id, fn)}
+                >
+                  <p>{team.departament}</p>
+                </ListItem>
+              )}
+            />
+          </StyledWrapper>
+          <StyledWrapper>
+            {!isNewTeam ? (
+              <InputSearch
+                name="category"
+                placeHolder="Category"
+                data={categories ? categories : []}
+                render={(category, fn) => (
+                  <ListItem
+                    onClick={() =>
+                      handleCategory(category.name, category.id, fn)
+                    }
+                  >
+                    <p>{category.name}</p>
+                  </ListItem>
+                )}
+              />
+            ) : (
+              <Input name="category" placeholder="Category" required />
+            )}
+          </StyledWrapper>
         </div>
+        <StyledWrapper>
+          <Input
+            name="taskName"
+            required
+            placeholder="Task Name"
+            setValue={setTaskName}
+          />
+        </StyledWrapper>
+        <StyledWrapper>
+          <Input
+            name="taskDescription"
+            required
+            placeholder="Task Description"
+            setValue={setTaskDescription}
+          />
+        </StyledWrapper>
 
-        {error && <div className='text-red-500 text-center mt-4'>{error}</div>}
-        <div className='flex justify-center min-t-[6vh] mt-4'>
-            <ButtonComponent size='large' variant='hover' type='submit' label="Create Task" />
-        </div>
+        <StyledWrapper>
+          <InputSearch
+            placeHolder="Priority"
+            data={priorities}
+            render={(priority, fn) => (
+              <ListItem
+                onClick={() => handlePriority(priority.name, priority.id, fn)}
+              >
+                <TaskPriority priority={priority.name} />
+              </ListItem>
+            )}
+          />
+        </StyledWrapper>
+      </div>
+
+      {error && <div className="text-red-500 text-center mt-4">{error}</div>}
+      <div className="flex justify-center min-t-[6vh] mt-4">
+        <ButtonComponent
+          size="large"
+          variant="hover"
+          type="submit"
+          label="Create Task"
+        />
+      </div>
     </form>
-);
+  );
 }
 
 export default TaskForm;
