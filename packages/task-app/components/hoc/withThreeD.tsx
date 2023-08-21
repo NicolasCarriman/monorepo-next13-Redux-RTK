93e18332a3f5ea4fd0ab9422187a9f115a1bde7a
NicolasCarriman <nicolascarriman045@gmail.com>
2023-08-21 11:33:34 -0300

    fix(taskform & dashboardmodal): fix ui from task form
    implements hoc that handles ui logic from preview task component

---
+++ packages/task-app/components/hoc/withThreeD.tsx
@@ -0,0 +1,113 @@
+/* eslint-disable react/display-name */
+import React, { forwardRef, useRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+import style from '../../app/style/cube.module.css';
+import Box from '../common/box';
+import { priorityType } from '@core/models';
+
+const FaceComponent: React.FC<React.ComponentProps<'div'>> = ({ className, ...rest }) => {
+  return (
+    <div
+      className={twMerge(`
+      absolute 
+      w-[400px] 
+      h-[400px] 
+      flex 
+      justify-center 
+      items-center 
+      `, className)}
+      {...rest}
+    />
+  );
+};
+
+interface CubeProps extends React.ComponentProps<'div'> {
+  firstFace: React.ReactNode;
+  secondFace: React.ReactNode;
+}
+
+type cubeRef = HTMLDivElement;
+
+export const CubePerspective = forwardRef<cubeRef, CubeProps>((
+  props,
+  ref
+) => {
+
+  const { firstFace, secondFace, ...rest } = props;
+
+  return (
+    <div ref={ref} className={style.cube} {...rest}>
+      <FaceComponent className={`${style.face} ${style.firstFace}`}>
+        {firstFace}
+      </FaceComponent>
+      <FaceComponent className={`${style.face} ${style.secondFace}`} />
+      <FaceComponent className={`${style.face} ${style.thirdFace}`} />
+      <FaceComponent className={`${style.face} ${style.fourthFace}`} >
+        {secondFace}
+      </FaceComponent>
+      <FaceComponent className={`${style.face} ${style.fifthFace}`} />
+      <FaceComponent className={`${style.face} ${style.sixthFace}`} />
+    </div>
+  );
+});
+
+interface WrappedFormProps {
+  next: () => void;
+  prev: () => void;
+}
+
+interface IForm {
+  closeModal: () => void;
+  setTaskName: React.Dispatch<React.SetStateAction<string>>;
+  setTaskDescription: React.Dispatch<React.SetStateAction<string>>;
+  setTaskPriority: React.Dispatch<React.SetStateAction<priorityType | string>>;
+  next: () => void;
+  prev: () => void;
+}
+
+function withThreeDForm<C extends React.JSX.IntrinsicAttributes, F>(
+  WrappedCube: React.ComponentType<C>,
+  WrappedForm: React.ComponentType<F & WrappedFormProps>,
+) {
+  const ComponentWithThreeD: React.FC<C & F> = (cubeProps: C, formProps: F) => {
+
+    const cubeRef = useRef<HTMLDivElement>(null);
+
+    const rotate = (degrees: number) => {
+      if (!cubeRef.current) return;
+
+      const cube = cubeRef.current;
+      cube.style.transition = 'all 1s ease-in-out';
+      cube.style.transform = `rotateY(${degrees}deg)`;
+    };
+
+    const nextView = () => {
+      rotate(-90);
+    };
+
+    const prevView = () => {
+      rotate(0);
+    };
+
+    const extraFormProps: WrappedFormProps = {
+      next: nextView,
+      prev: prevView
+    };
+
+
+    return (
+      <>
+        <Box>
+          <WrappedForm {...formProps} {...extraFormProps} />
+        </Box>
+        <Box className='h-auto flex justify-center items-center'>
+          <WrappedCube ref={cubeRef} {...cubeProps} />
+        </Box>
+      </>
+    );
+  };
+
+  return ComponentWithThreeD;
+}
+
+export default withThreeDForm;
